75a647282bdd8307369c46e822f72320ba7fbbd4
Issac <issaclaikoiyit@gmail.com>
2024-11-19 02:29:04 +0800

    Adding and Applying CustToDate methods.

--- BusTimingTracker-Files/codeV2.js
+++ BusTimingTracker-Files/codeV2.js
@@ -4,8 +4,28 @@
 const inputSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Form Responses 1")
 const varSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Var Sheet")
 
+const CustDateTo = {
+    MMDDYYYY(dateStr = "") {
+        dateStr = new Date(dateStr)
+        return `${dateStr.getMonth() + 1}/${dateStr.getDate()}/${dateStr.getFullYear()}` 
+        //the +1 for month is because either JS or AppScript uses a 0 index for months (but not dates)
+    },
+
+    HHMM(dateStr = "") {
+        dateStr = new Date(dateStr)
+        let HH = dateStr.getHours()
+        let MM = dateStr.getMinutes()
+
+        // converting into string so that when filled into varSheet, it is recognised as a time of day 
+        if (HH < 10) {HH = `0${HH}`} else {HH = `${HH}`}
+        if (MM < 10) {MM = `0${MM}`} else {MM = `${MM}`}
+
+        return `${HH}:${MM}`
+    }
+}
+
 const formValues = {
-    Dates: inputSheet.getRange("A2:A").getValues().filter((formDate) => {return formDate != ''}),
+    DateNTimes: inputSheet.getRange("A2:A").getValues().filter((formDate) => {return formDate != ''}),
     TravelEvents: inputSheet.getRange("B2:B").getValues().filter((formEvent) => {return formEvent != ''}),
     UsrTimes: inputSheet.getRange("C2:C").getValues().filter((formUsrTimes) => {return formUsrTimes != ''})
 }
@@ -20,8 +40,8 @@ function GetNewEntriesStartIndex() {
     //gets the index of the first new entry in the array formDate. 
     //a new entry is determined as the entry that is not already recorded in Var Sheet. 
     
-    let varDateStrings = varValues.Dates.map((varDate) => {varDate = new Date(varDate); return varDate.toLocaleDateString()})
-    let formDateStrings = formValues.Dates.map((formDate) => {formDate = new Date(formDate); return formDate.toLocaleDateString()})
+    let varDateStrings = varValues.Dates.map((varDate) => {varDate = new Date(varDate); return CustDateTo.MMDDYYYY(varDate)})
+    let formDateStrings = formValues.DateNTimes.map((formDate) => {formDate = new Date(formDate); return CustDateTo.MMDDYYYY(formDate)})
     
     if (varDateStrings.length != 0) { 
         let lastVarDate = varDateStrings[varDateStrings.length - 1]
@@ -31,7 +51,7 @@ function GetNewEntriesStartIndex() {
     }
 }
 
-function GetUpNLowBounds(dataPoint, formTiming) {
+function GetUpNLowBounds(dataPoint = "", formTiming = "") {
     dataPoint = new Date(dataPoint)
     formTiming = new Date(formTiming)
 
@@ -58,7 +78,7 @@ function GetUpNLowBounds(dataPoint, formTiming) {
     let upperBoundInMin = dataPointInMin + dataPointInMin * percentageErr()
     let lowerBoundInMin = dataPointInMin - dataPointInMin * percentageErr()
     
-    function ToHHMM(valueInMin) {
+    function ToHHMM(valueInMin = 0) {
         return `${Math.floor(valueInMin / 60)}:${(valueInMin / 60 - Math.floor(valueInMin / 60)) * 60}`
     }
 
@@ -75,23 +95,25 @@ function FillTables() {
         UsrTimes:[]
     }
 
-    formValues.Dates.forEach((formDate, i) => {if (i >= newEntries.StartIndex) { formDate = new Date(formDate); newEntries.Dates.push(formDate)}})
-    newEntries.Timings = newEntries.Dates.map((newFormDate) => {newFormDate = new Date(newFormDate); return newFormDate.toLocaleTimeString()})
-    // there may be issues with the never property              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+    formValues.DateNTimes.forEach((formData, i) => {if (i >= newEntries.StartIndex) { 
+        formData = new Date(formData); 
+        newEntries.Dates.push(CustDateTo.MMDDYYYY(formData)); 
+        newEntries.Timings.push(CustDateTo.HHMM(formData))
+    }})
     formValues.TravelEvents.forEach((travelEvent, i) => {if (i >= newEntries.StartIndex) {newEntries.TravelEvents.push(travelEvent)}})
-    formValues.UsrTimes.forEach((formUsrTime, i) => {if (i >= newEntries.StartIndex) {newEntries.UsrTimes.push(formUsrTime)}})
+    formValues.UsrTimes.forEach((formUsrTime, i) => {if (i >= newEntries.StartIndex) {newEntries.UsrTimes.push(CustDateTo.HHMM(formUsrTime))}})
     
-    function GetUniqueDates(rawDates) {
+
+    function GetUniqueDates(rawDates = "") {
         let output = []
-        
-        rawDates.forEach((raw, i) => {
+
+        rawDates.forEach((raw) => {
+            raw = CustDateTo.MMDDYYYY(raw)
+            
             let isMatch = false
+            output.forEach((stored) => {if (raw == stored) {isMatch = true}})
 
-            output.forEach((stored, j) => {if (raw == stored) {isMatch = true; return}})
-
-            if (!isMatch){
-                output.push(raw)
-            } 
+            if (!isMatch) {output.push(raw)} 
         })
 
         return output
@@ -99,22 +121,22 @@ function FillTables() {
 
     let newVarValues = {
         StartRow: varValues.Dates.length + 5,
-        Dates: GetUniqueDates(newEntries.Dates).map((dateStr) => {dateStr = new Date(dateStr); return `${dateStr.getMonth()}/${dateStr.getDate()}/${dateStr.getFullYear()}`}),
+        Dates: GetUniqueDates(newEntries.Dates),
         TEColumns: ["B","E","H","K"],
 
         //I can't use getRow() and getColumn() because those are methods in appscript
-        getYCoord(date) {
+        getYCoord(date = "") {
             return this.Dates.indexOf(date) + this.StartRow
         },
 
-        getXCoord(travelEvent) {
-            return TEColumns[varValues.TravelEvents.indexOf(travelEvent)]
+        getXCoord(travelEvent = "") {
+            return this.TEColumns[varValues.TravelEvents.indexOf(travelEvent)]
         }
     }
 
     console.log('var newVarValues :>> ', newVarValues);
 
-    for (i = 0 ; i < newEntries.Dates.length; i) {
+    for (let i = 0 ; i < newEntries.Dates.length; i) {
         let dataPoint = {
             Values: GetUpNLowBounds(newEntries.UsrTimes[i], newEntries.Timings[i]),
             Date: newEntries.Dates[i],
@@ -138,7 +160,7 @@ function TestingGrounds() {
     formUsrTimes.forEach((formUsrTime, i) => {if (i >= formStartIndex) {newFormUsrTimes.push(formUsrTime)}})
 
     let newUniqueDates = new Set(newFormDates)
-    for (i = 0; i < newFormDates.length; i++) {
+    for (let i = 0; i < newFormDates.length; i++) {
         varSheet.getRange(`A${5 + varValues.Dates.length + i}`).setValue(newUniqueDates[i])
     }
 }
